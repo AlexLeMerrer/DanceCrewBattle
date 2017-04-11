82c10f2b6ca35d32c939b1bdf8d9fcff7ee1131b
Chavatanak <chavatanak.ving@gmail.com>
2017-04-11 19:39:26 +0200

    animation player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,39 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GAF.Core;
 
 public class Player : MonoBehaviour {
 
+    public delegate void State();
+    public State behaviour;
+
+    //Liste des joueurs
     public static List<Player> list = new List<Player>();
     public float velocity = 3;
     public float gainInfluence = 1f;
     public InfluenceCircle influenceAsset;
-
-
+    public GAFMovieClip animationAsset;
+    
     // TO MOVE
     private float topLimit;
     private float bottomLimit;
     private float rightLimit;
     private float leftLimit;
 
-    // Use this for initialization
+    private float toleranceMove = .1f;
+
     void Start () {
         list.Add(this);
 
-        topLimit = Camera.main.orthographicSize                         - GetComponent<Renderer>().bounds.size.y;
-        bottomLimit = -Camera.main.orthographicSize                     + GetComponent<Renderer>().bounds.size.y;
-        rightLimit = Camera.main.orthographicSize * Camera.main.aspect  - GetComponent<Renderer>().bounds.size.x;
-        leftLimit = -Camera.main.orthographicSize * Camera.main.aspect  + GetComponent<Renderer>().bounds.size.x;
+        topLimit =      Camera.main.orthographicSize                        - GetComponent<Renderer>().bounds.size.y;
+        bottomLimit =   -Camera.main.orthographicSize                       + GetComponent<Renderer>().bounds.size.y;
+        rightLimit =    Camera.main.orthographicSize * Camera.main.aspect   - GetComponent<Renderer>().bounds.size.x;
+        leftLimit =     -Camera.main.orthographicSize * Camera.main.aspect  + GetComponent<Renderer>().bounds.size.x;
+
+        setModeIdle();
     }
 	
-	// Update is called once per frame
+
 	void Update ()
     {
-        Move();
-        GrowInfluence();
+        ControlMove();
+        CheckInfluence();
+        behaviour();
     }
 
-    private void Move()
+    #region State Machine
+        #region idle
+        private void setModeIdle()
+        {
+            behaviour = Idle;
+            animationAsset.GetComponent<GAFMovieClip>().setSequence("idle", true);
+
+        }
+        private void Idle()
+        {
+            if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+                setModeMove();
+        }
+        #endregion
+        #region Move
+        private void setModeMove()
+        {
+            behaviour = Move;
+            animationAsset.GetComponent<GAFMovieClip>().setSequence("move", true);
+
+        }
+        private void Move()
+        {   
+            //if (Input.GetAxis("Vertical") == 0 || Input.GetAxis("Horizontal") == 0)
+            //    setModeIdle();
+        }
+        #endregion
+    #endregion
+
+    #region Methods
+    private void ControlMove()
     {
         Vector3 newPos = transform.position + Time.fixedDeltaTime * velocity * new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position = new Vector3(Mathf.Clamp(newPos.x, leftLimit, rightLimit),
@@ -41,10 +80,12 @@ public class Player : MonoBehaviour {
                                          newPos.z);
     }
 
-    private void GrowInfluence ()
+    private void CheckInfluence()
     {
         if(Input.GetButtonDown("Fire1"))
             influenceAsset.SetModeGrow(gainInfluence);
     }
+    
+    #endregion
 
 }
